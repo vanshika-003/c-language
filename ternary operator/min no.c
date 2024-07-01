0ad8e39f611eac41997b35abf12b290f83e15d25
vanshika-003 <sarojvanshika224@gmail.com>
2024-07-01 11:45:56 +0530

    Add files via upload

---
+++ ternary operator/min no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h> 
+
+void main()
+{
+	int x;
+	int y;
+	printf("enter the value of x:");
+	scanf("%d",&x);
+	printf("enter the value of y:");
+	scanf("%d",&y);
+	(8<3 )?printf("the minimum number is 8"):printf("the minimum number is 3");
+}
