5e60512847396eb41d786eb1bf913e9ac2950bb0
vanshika-003 <sarojvanshika224@gmail.com>
2024-07-01 12:10:26 +0530

    Add files via upload

---
+++ variableandconstant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y;
+	x=15;
+	y=10;
+	printf("%d+%d=%d\n",x,y,x+y);
+	printf("%d-%d=%d\n",x,y,x-y);
+	printf("%d*%d=%d\n",x,y,x*y);
+	printf("%d/%d=%d\n",x,y,x/y);
+}
