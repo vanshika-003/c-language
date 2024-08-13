b390aeae41fc5c13bd9ea22e9676c07e11339a88
vanshika-003 <sarojvanshika224@gmail.com>
2024-08-13 10:02:50 +0530

    Add files via upload

---
+++ parten/p11.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=i;j<=5;j++)
+		{
+			if(j%2==0)
+			{
+				printf("0 ");
+			}
+			else
+			{
+				printf("1 ");
+			}
+		}
+	printf("\n");
+    }
+}
---
+++ parten/p13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char i,j,k='A';
+	for(i='A';i<='E';i++)
+	{
+		for(j='A';j<=i;j++)
+		{
+			printf("%c ",k);
+			k++;
+		}
+		printf("\n");
+	}
+}
